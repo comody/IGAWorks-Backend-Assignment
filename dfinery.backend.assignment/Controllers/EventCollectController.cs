1a74de34f08e806878380613afc1653d803f864c
comody <comody224@gmail.com>
2022-03-27 14:30:03 +0900

    feat: implement event collect controller

---
+++ dfinery.backend.assignment/Controllers/EventCollectController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using dfinery.backend.assignment.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dfinery.backend.assignment.Service.Messenger;
+
+namespace dfinery.backend.assignment.Controllers
+{
+    [Route("api/collect")]
+    public class EventCollectController : Controller 
+    {
+        private readonly IEventSQSMessenger eventSQSMessenger;
+
+        public EventCollectController(IEventSQSMessenger eventSQSMessenger)
+        {
+            this.eventSQSMessenger = eventSQSMessenger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CollectEventAsync([FromBody] EventCollectReq req)
+        {
+            bool is_success = true;
+
+            EventCollectModel eventCollectModel = req.ToEventModel();
+
+            try
+            {
+                await eventSQSMessenger.SendMessage(eventCollectModel);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error pushing message to sqs : {e.Message}");
+                is_success = false;
+            }
+            return Ok(new EventCollectRes { is_success = is_success });
+
+        }
+    }
+}
