8cb294af2b01dbb6a605b7de2f1cbbf551573bc2
comody <comody224@gmail.com>
2022-04-02 15:51:58 +0900

    fix: change primary key, add index

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `event_database`.`event_tb` (
   `event_id` VARCHAR(50) NOT NULL,
   `user_id` VARCHAR(50) NOT NULL,
-  `event` VARCHAR(50) NULL,
+  `event` VARCHAR(50) NOT NULL,
   `parameters` BLOB NULL,
-  `event_datetime` DATETIME(3) NULL,
-  PRIMARY KEY (`event_id`, `user_id`));
+  `event_datetime` DATETIME(3) NOT NULL,
+  PRIMARY KEY (`event_id`));
+
+ALTER TABLE event_tb ADD INDEX idx_user(user_id);
